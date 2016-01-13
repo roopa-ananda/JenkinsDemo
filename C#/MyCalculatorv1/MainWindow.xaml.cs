d4f1555c1240715b8d06de9a6dab31d706e72455
Krithivasan B <Krithivasan B>
2016-01-13 17:39:06 -0600

    test build1

--- C#/MyCalculatorv1/MainWindow.xaml.cs
+++ C#/MyCalculatorv1/MainWindow.xaml.cs
@@ -62,7 +62,7 @@ namespace MyCalculatorVersion1
             op = tb.Text.Substring(iOp, 1);
             double op1 = Convert.ToDouble(tb.Text.Substring(0, iOp));
             double op2 = Convert.ToDouble(tb.Text.Substring(iOp + 1, tb.Text.Length - iOp - 1));
-            tb.Text += "=" + businessAction.FetchResult(op, op1, op2);
+            tb.Text += " = " + businessAction.FetchResult(op, op1, op2);
         }
 
         private void Off_Click_1(object sender, RoutedEventArgs e)
